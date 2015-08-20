238439d4b81ded743d5e7cdf3a8535f836018cd1
Anthony Emberley <emberley@mit.edu>
2015-08-20 13:23:44 -0400

    first commit

---
+++ Impact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Impact
+//
+//  Created by Anthony Emberley on 8/20/15.
+//  Copyright (c) 2015 Impact. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
