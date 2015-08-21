84ce134af37f4ba256cf158ae121b98d96d2b90b
Phillip Ou <pou@mit.edu>
2015-08-21 01:04:37 -0700

    mocked up server singleton

---
+++ Impact/ServerRequest.swift
@@ -0,0 +1,64 @@
+//
+//  ServerRequest.swift
+//  Impact
+//
+//  Created by Phillip Ou on 8/20/15.
+//  Copyright (c) 2015 Impact. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ServerRequest: NSObject {
+    let baseURL = "";
+    static let shared = ServerRequest();
+    
+    //MARK :  Helper Methods
+    private func getWithEndpoint(endpoint:String, parameters:[String : AnyObject]?, authenticated:Bool, success:(json:String) -> Void, failure:(error:String) -> Void) {
+        let path : String = baseURL + endpoint;
+        let manager = getRequestManager(authenticated);
+        Alamofire.request(.GET, path, parameters: parameters).responseJSON{ (request, response, dataResponse, error) -> Void in
+            
+        };
+    }
+    
+    private func updateWithEndpoint(endpoint:String, parameters:[String : AnyObject]?, authenticated:Bool, success:(json:String) -> Void, failure:(error:String) -> Void) {
+        let path : String = baseURL + endpoint;
+        let manager = getRequestManager(authenticated);
+        Alamofire.request(.PUT, path, parameters: parameters).responseJSON{ (request, response, dataResponse, error) -> Void in
+            
+        };
+        
+    }
+    
+    private func postWithEndpoint(endpoint:String, parameters:[String : AnyObject]?, authenticated:Bool, success:(json:String) -> Void, failure:(error:String) -> Void) {
+        let path : String = baseURL + endpoint;
+        let manager = getRequestManager(authenticated);
+        Alamofire.request(.POST, path, parameters: parameters).responseJSON{ (request, response, dataResponse, error) -> Void in
+            
+        };
+        
+    }
+    
+    private func deleteWithEndoint(endpoint:String, parameters:[String : AnyObject]?, authenticated:Bool, success:(json:String) -> Void, failure:(error:String) -> Void) {
+        let path : String = baseURL + endpoint;
+        let manager = getRequestManager(authenticated);
+        Alamofire.request(.DELETE, path, parameters: parameters).responseJSON{ (request, response, dataResponse, error) -> Void in
+            
+        };
+        
+    }
+    
+    private func getAuthenticationToken() -> String? {
+        return "";
+    }
+    
+    private func getRequestManager(authenticated:Bool) -> Manager {
+        var manager = Manager.sharedInstance
+        manager.session.configuration.HTTPAdditionalHeaders = ["Content-Type" : "application/json", "Accept": "application/json"];
+        if (authenticated && let api_token = getAuthenticationToken()) {
+            manager.session.configuration.HTTPAdditionalHeaders = ["Content-Type" : "application/json", "Accept" : "application/json", "API-TOKEN" : api_token];
+        }
+        return manager;
+    }
+}
