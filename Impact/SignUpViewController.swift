d8f1cf71bdd0fd288495566691aea99aec13a043
Phillip Ou <pou@mit.edu>
2015-10-08 11:26:59 -0400

    successfully retrieved banks

--- Impact/SignUpViewController.swift
+++ Impact/SignUpViewController.swift
@@ -82,13 +82,14 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func createAccountButtonPressed(sender: AnyObject) {
         let payload = ["name":fullNameTextField.text!, "password": passwordTextField.text!, "email": emailTextField.text!]
-        ServerRequest.shared.signUpWithPayload(payload, success: { (json) -> Void in
-            self.navigateToBankViewController()
-            },failure: { (errorMessage) -> Void in
-                let alertController = AlertViewController()
-                alertController.setUp(self, title: "Error", message: errorMessage, buttonText: "Dismiss")
-                alertController.show()
-        })
+//        ServerRequest.shared.signUpWithPayload(payload, success: { (json) -> Void in
+//
+//            },failure: { (errorMessage) -> Void in
+//                let alertController = AlertViewController()
+//                alertController.setUp(self, title: "Error", message: errorMessage, buttonText: "Dismiss")
+//                alertController.show()
+//        })
+        self.navigateToBankViewController()
     }
     
     func navigateToBankViewController() {
