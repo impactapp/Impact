a9b8df333d08178157f9b57631e4681fc5876bdd
Phillip Ou <pou@mit.edu>
2016-02-15 21:17:53 -0500

    fixed tab bar icons

---
+++ Impact/DonateViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DonateViewController.swift
+//  Impact
+//
+//  Created by Phillip Ou on 2/15/16.
+//  Copyright © 2016 Impact. All rights reserved.
+//
+
+import UIKit
+
+class DonateViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Impact/DonationCardView.swift
@@ -0,0 +1,21 @@
+//
+//  DonationCardView.swift
+//  Impact
+//
+//  Created by Phillip Ou on 2/15/16.
+//  Copyright © 2016 Impact. All rights reserved.
+//
+
+import UIKit
+
+class DonationCardView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
