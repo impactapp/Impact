c8d05f0308f347a7449b406b62614413d9a906fb
Phillip Ou <pou@mit.edu>
2016-02-16 20:22:48 -0500

    got nib to display properly

--- Impact/DonateViewController.swift
+++ Impact/DonateViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class DonateViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    let donateCardViewHeight = CGFloat(225)
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        self.tableView.backgroundColor = UIColor.customDarkGrey()
         
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        let footer = UIView(frame: CGRectMake(0,0,self.view.frame.size.width,400))
-        
-        let header = DonationCardView(frame: CGRectMake(0,0,footer.frame.size.width,300))
-        footer.addSubview(header)
+        let footer = UIView(frame: CGRectMake(0,0,self.view.frame.size.width,420))
         self.tableView.tableFooterView = footer
+        let inset = CGFloat(8)
+        let cardViewWidth = self.view.frame.size.width - 2*inset
+        let donationCardView = DonationCardView(frame: CGRectZero)
+        footer.addSubview(donationCardView)
+        donationCardView.frame = CGRectMake(inset,20,cardViewWidth,donateCardViewHeight)
+        donationCardView.layer.masksToBounds = true
+        donationCardView.layer.cornerRadius = 10
+
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
